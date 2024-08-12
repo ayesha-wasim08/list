8210771ebcefb6482ac21f8fda9267dd28d66646
ayesha-wasim08 <ashisiddique290@gmail.com>
2024-08-12 13:13:22 +0500

    firstcommit

---
+++ app.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded",function() {
+    let songlist = document.querySelectorAll("#song-list li");
+    let audio = document.getElementById("audio");
+    let currentsong  = document.getElementById("current-song");
+
+    songlist.forEach(function(songitem){
+        songitem.addEventListener("click", function(){
+            let song = songitem.getAttribute("data-song")
+            audio.src = song
+            audio.play()
+            currentsong.textContent = songitem.textContent
+        })
+    })
+
+})
+// document.addEventListener('DOMContentLoaded', function(){
+//     let songlist = document.querySelectorAll("#song-list li");
+//     let audio = document.getElementById("audio");
+//     let currentsong = document.getElementById("current-song");
+
+//     songlist.forEach(function(songitem) {
+//         songitem.addEventListener("click", function(){
+//             let song = songitem.getAttribute("data-song");
+//             audio.src = song;
+//             currentsong.textContent = songitem.textContent;
+//         });
+//     });
+// });
